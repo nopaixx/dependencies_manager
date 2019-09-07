0923b321a6c025c3fc1712547fa0b9faa46f5373
angel <nopaixx@gmail.com>
2019-09-07 11:56:31 +0200

    running commands

--- project/system_commands/depend_class.py
+++ project/system_commands/depend_class.py
@@ -1,15 +1,31 @@
 import os
-
+import subprocess
+from project.constants import c_DEPEND
 
 class DEPEND():
-
+    
     def __init__(self, action):
         self.action = action
+        self.system_action_name = c_DEPEND
+
+    def check(self, depend_name):
+        """
+        ALORDAN
+        this method recibe a dependencie and check if installed on system
+        """
+        result = subprocess.run(['which', depend_name], stdout=subprocess.PIPE)
+        if len(result.stdout)>0:
+            return True
+        else:
+            return False
 
     def run(self):
         """ALORDAN
-        this class know how to install dependencies
+        this class know how to check dependencies
         self.actions is and array of dependencies to install
         """
-        print("debug", self.action) 
-        pass
+
+        # ?
+        self.check(self.action[0])
+        print ("{} {}".format(self.system_action_name, " ".join(self.action)))
+
